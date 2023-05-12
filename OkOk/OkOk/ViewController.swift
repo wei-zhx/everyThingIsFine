d3fd32fa5784b8e0beb152ece22bac06abed19ba
wei <Charles_wei_1991@126.com>
2023-05-12 10:19:47 +0800

    create Demo

---
+++ OkOk/OkOk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OkOk
+//
+//  Created by biprogybank01 on 2023/5/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
